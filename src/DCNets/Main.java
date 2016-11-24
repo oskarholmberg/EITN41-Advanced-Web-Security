d16a266645942d65ec85fbbc2cdacd3d3d09c138
oskarholmberg <dat12oho@student.lu.se>
2016-11-24 14:13:23 +0100

    Handle wrongful input better for DCNets and init DisclosureAttack

--- src/DCNets/Main.java
+++ src/DCNets/Main.java
@@ -1,22 +1,31 @@
 package DCNets;
 
-import java.io.UnsupportedEncodingException;
+import com.sun.org.apache.xpath.internal.SourceTree;
 
 /**
  * Created by oskar on 2016-11-24.
  */
 public class Main {
-    public static void main(String[] args){
-        int sa = Integer.parseInt(args[0], 16);
-        int sb = Integer.parseInt(args[1], 16);
-        int da = Integer.parseInt(args[2], 16);
-        int db = Integer.parseInt(args[3], 16);
-        int m = Integer.parseInt(args[4], 16);
-        int b = Integer.parseInt(args[5]);
-        if (b==0){
-            System.out.println(Integer.toHexString(sa^sb) + Integer.toHexString(sa^sb^da^db));
-        } else {
-            System.out.println(Integer.toHexString(sa^sb^m));
+    public static void main(String[] args) {
+        if (args.length == 6) {
+            int sa = Integer.parseInt(args[0], 16);
+            int sb = Integer.parseInt(args[1], 16);
+            int da = Integer.parseInt(args[2], 16);
+            int db = Integer.parseInt(args[3], 16);
+            int m = Integer.parseInt(args[4], 16);
+            int b = Integer.parseInt(args[5]);
+            if (b == 0) {
+                System.out.println(Integer.toHexString(sa ^ sb) + Integer.toHexString(sa ^ sb ^ da ^ db));
+            }
+            else if (b == 1){
+                System.out.println(Integer.toHexString(sa ^ sb ^ m));
+            }
+            else {
+                System.out.println("Invalid input, try again");
+            }
+        }
+        else {
+            System.out.println("Not enough input parameters. Was " + args.length + ", should be 6.");
         }
     }
 }
---
+++ src/DisclosureAttack/Main.java
@@ -0,0 +1,10 @@
+package DisclosureAttack;
+
+/**
+ * Created by oskar on 2016-11-24.
+ */
+public class Main {
+    public static void main(String[] args){
+        
+    }
+}
