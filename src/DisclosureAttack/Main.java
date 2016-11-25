a0afca0e13c11eeb53cd2a4a51ea65c274014086
erikmunkby <erik.munkby@gmail.com>
2016-11-25 10:52:07 +0100

    added exclusion stage, something not working

--- src/DisclosureAttack/Main.java
+++ src/DisclosureAttack/Main.java
@@ -4,6 +4,7 @@ package DisclosureAttack;
 import java.util.ArrayList;
 import java.util.BitSet;
 import java.util.SplittableRandom;
+import java.util.stream.Collectors;
 
 /**
  * Created by oskar on 2016-11-24.
@@ -19,6 +20,7 @@ public class Main {
         amountUsers = Integer.valueOf(args[0]);
         int aliceFreq = Integer.valueOf(args[1]);
         aliceAmountRecs = Integer.valueOf(args[2]);
+        ArrayList<BitSet> mdBatches = new ArrayList<>();
         User[] users = new User[amountUsers];
         for (int i = 0; i < amountUsers - 1; i++) {
             users[i] = new User(i, getRecievers(false), random.nextInt(1000, 1500), random);
@@ -26,11 +28,11 @@ public class Main {
         users[amountUsers - 1] = new User(amountUsers - 1, getRecievers(true), aliceFreq, random);
         System.out.println("System has " + amountUsers + " users. Alice sends messages to " + aliceAmountRecs + " other users.");
         long t1, t2 = System.currentTimeMillis();
-        int mailsSent = 0, counter = 0;
-        boolean aliceSent = false, done = false;
+        int mailsSent = 0, counter = 0, stage=1;
+        boolean aliceSent = false;
         BitSet batch = new BitSet(amountUsers);
         ArrayList<BitSet> batches = new ArrayList<>();
-        while (!done) {
+        while (stage!=3) {
             t1 = System.currentTimeMillis();
             while (System.currentTimeMillis() < (t1 + 1)) {
                 for (int i = 0; i < amountUsers - 1; i++) {
@@ -48,22 +50,73 @@ public class Main {
                 }
             }
             if(aliceSent){
-                batches.add(batch);
+                if (stage==1){
+                    batches.add(batch);
+                } else if (stage==2){
+                    batches = checkExclusion(mdBatches, batch);
+                    if (!batches.isEmpty()){
+                        mdBatches = batches;
+                        if (checkLengths(mdBatches)){
+                            stage=3;
+                        }
+                    }
+                }
                 if (System.currentTimeMillis() > (t2+3000)){
                     System.out.println("Mails sent: " + mailsSent + " snapshot batch: " + batch.toString());
                     t2=System.currentTimeMillis();
+                    for(BitSet b : mdBatches){
+                        System.out.print(b.cardinality() + " ");
+                    }
+                    System.out.println();
                 }
             }
             aliceSent = false;
             batch = new BitSet(amountUsers);
 
             counter++;
-            if (counter > 100){
-                if (!mutuallyDisjoint(batches, aliceAmountRecs).isEmpty())
-                    done=true;
+            if (counter > 100 && stage==1){
+                ArrayList<Integer> indices = mutuallyDisjoint(batches, aliceAmountRecs);
+                if (!indices.isEmpty()) {
+                    stage = 2;
+                    mdBatches.addAll(indices.stream().map(batches::get).collect(Collectors.toList()));
+                    for (BitSet b : mdBatches){
+                        System.out.print(b.cardinality() + " ");
+                        System.out.println();
+                    }
+                }
                 counter=0;
             }
         }
+        System.out.println("Done! Total amount of mails sent: " + mailsSent);
+    }
+
+    public static boolean checkLengths(ArrayList<BitSet> mdBatches){
+        for (BitSet b : mdBatches){
+            if (b.cardinality()>1){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static ArrayList<BitSet> checkExclusion(ArrayList<BitSet> mdBatches, BitSet batch){
+        boolean andIt = true;
+        for (int i = 0; i < mdBatches.size(); i++){
+            if (mdBatches.get(i).intersects(batch)) {
+                for (int j = 0; j < mdBatches.size(); i++) {
+                    if (i != j && mdBatches.get(j).intersects(batch))
+                        andIt = false;
+                }
+            }
+            if (andIt){
+                mdBatches.get(i).and(batch);
+                System.out.println(batch);
+                return mdBatches;
+            }
+            andIt = true;
+        }
+        mdBatches.clear();
+        return mdBatches;
     }
 
     public static int[] getRecievers(boolean alice) {
