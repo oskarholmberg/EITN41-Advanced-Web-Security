16319e13ec1d2a1ab2571ed21fa438e4dba46691
erikmunkby <erik.munkby@gmail.com>
2016-12-05 13:11:23 +0100

    added comments

--- src/CommitmentScheme/Main.java
+++ src/CommitmentScheme/Main.java
@@ -7,7 +7,7 @@ import java.util.SplittableRandom;
  * Created by erik on 30/11/16.
  */
 public class Main {
-    // enda inputen är om valet är 0 eller 1, bit-längden är fix 16
+    // only input is if the vote was 0 or 1 from start, k-bit length is fix to 16
     public static void main(String[] args){
         BigInteger v = new BigInteger(args[0]);
         int k = 16;
@@ -16,7 +16,7 @@ public class Main {
         runProbabilities(v, kString);
     }
 
-    // här körs allting och data ska samlas, testa mellan alla bit-strängar från
+    // main loop, tests with bit strings from
     // 0000000000000000 till 1111111111111111
     private static void runProbabilities(BigInteger v, String kString){
         BigInteger originalHash = hashFunction(v, kString);
@@ -29,13 +29,14 @@ public class Main {
             BigInteger noVote = hashFunction(new BigInteger("0"), testKString);
             if (yesVote.compareTo(noVote) == 0){
                 // this means that the concealement is better
+                // might need to be tested 2^16^2 times :S
             }
 
             BigInteger correctVote = hashFunction(v, testKString);
             // with vote 1-v (0 if 1, 1 if 0)
             BigInteger changeVote = hashFunction((new BigInteger("1").add(v.negate())), testKString);
             if (correctVote.compareTo(changeVote) == 0){
-                // detta betyder att alice kan säga att hennes röst var en annan
+                // this means that alice can claim that her vote was a different one
             }
         }
     }
@@ -44,7 +45,7 @@ public class Main {
         return prevTruncation*10;
     }
 
-    // skapa nästa Kstring att testa mot (minsta längd alltid 16 bitar)
+    // create k-string, bit length is always 16
     private static String nextKString(int value){
         String kString = Integer.toBinaryString(value);
         while (kString.length() > 16){
@@ -53,7 +54,7 @@ public class Main {
         return kString;
     }
 
-    //skapa random kstring att hasha med
+    //create random k-string
     private static String generateRandomKstring(int bitLength){
         SplittableRandom random = new SplittableRandom();
         String kString = Integer.toBinaryString(random.nextInt(0, (int) Math.pow(2, bitLength)));
@@ -63,8 +64,8 @@ public class Main {
         return kString;
     }
 
-    //simpel hash funktion. stora primtal för att kunna använda längre trunkeringar
-    // (storleken är dragna ur röven, kanske ska ändra storleken till något rimligare (read: mindre))
+    //simple hash function, big primes in order to be able to use longer truncations
+    // (the primes are conjured from nothing, probably change them to something more reasonable (read: smaller))
     private static BigInteger hashFunction(BigInteger v, String kString){
         BigInteger hash = new BigInteger("1601");
         hash = hash.multiply(new BigInteger("1153")).add(v);
