689625e578483dd4bc699bf7fa61fde4a8cbd053
oskarholmberg <dat12oho@student.lu.se>
2016-12-13 16:19:51 +0100

    asd

--- src/SignatureCalc/Main.java
+++ src/SignatureCalc/Main.java
@@ -1,22 +1,26 @@
 package SignatureCalc;
 
+import javax.net.ssl.HttpsURLConnection;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
+import java.net.*;
 
 /**
  * Created by oskar on 2016-12-13.
  */
 public class Main {
-    public static void main(String[] args){
-        String address = "https://eitn41.eit.lth.se:3119/ha4/addgrade.php?name=" + args[0]+"&grade="+args[1]+"&signature=";
+    public static void main(String[] args) {
+        String address = "https://eitn41.eit.lth.se:3119/ha4/addgrade.php?name=" + args[0] + "&grade=" + args[1] + "&signature=";
+        URL url;
         try {
-            URL url =  new URL(address);
-            URLConnection connection = url.openConnection();
-            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            HttpsURLConnection con;
+            url = new URL(address);
+            con = (HttpsURLConnection) url.openConnection();
+            BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
