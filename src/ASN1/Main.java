14f5d47011d5b5ffd2cb72974bf7e0984e9e74ae
erikmunkby <erik.munkby@gmail.com>
2016-12-20 13:44:39 +0100

    part two should work now as well, but it doesn't

--- src/ASN1/Main.java
+++ src/ASN1/Main.java
@@ -2,12 +2,54 @@ package ASN1;
 
 import javax.xml.bind.DatatypeConverter;
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Base64;
 
 /**
  * Created by oskar on 2016-12-20.
  */
 public class Main {
+    private static BigInteger ONE = new BigInteger("1");
     public static void main(String[] args){
-        System.out.println(DatatypeConverter.printHexBinary(DER.encode(new BigInteger(args[0].replace(" ", "")))));
+        if (args.length==1) {
+            System.out.println(DatatypeConverter.printHexBinary(DER.encode(new BigInteger(args[0].replace(" ", "")))));
+        } else {
+            BigInteger p = new BigInteger(args[0]);
+            BigInteger q = new BigInteger(args[1]);
+            BigInteger e = new BigInteger("65537");
+            BigInteger n = p.multiply(q);
+            BigInteger phi = (p.subtract(ONE)).multiply((q.subtract(ONE)));
+            BigInteger d = e.modInverse(phi);
+            BigInteger exp1 = d.mod(p.subtract(ONE));
+            BigInteger exp2 = d.mod(q.subtract(ONE));
+            BigInteger coeff = q.modInverse(p);
+            ArrayList<byte[]> derCodes = new ArrayList<>();
+            derCodes.add(DER.encode(n));
+            derCodes.add(DER.encode(e));
+            derCodes.add(DER.encode(d));
+            derCodes.add(DER.encode(p));
+            derCodes.add(DER.encode(q));
+            derCodes.add(DER.encode(exp1));
+            derCodes.add(DER.encode(exp2));
+            derCodes.add(DER.encode(coeff));
+            int totLength = 0;
+            for (byte[] b : derCodes){
+                totLength+=b.length;
+            }
+            byte[] content = new byte[5+totLength];
+            content[0] = 0x30;
+            content[1] = (byte) (3+totLength);
+            content[2] = 0x02;
+            content[3] = 0x01;
+            content[4] = 0x00;
+            int offset = 5;
+            for (byte[] b : derCodes){
+                System.arraycopy(b, 0, content, offset, b.length);
+                offset+=b.length;
+            }
+            System.out.println(DatatypeConverter.printHexBinary(content));
+            System.out.println(DatatypeConverter.printBase64Binary(content));
+
+        }
     }
 }
